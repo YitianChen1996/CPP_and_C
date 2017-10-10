8daffe0e3b36f20ec442d8b0d9812b451af22c47
YitianChen1996 <30911432+YitianChen1996@users.noreply.github.com>
2017-10-10 12:22:29 +0000

    Add files via upload

---
+++ Interprocess synchronization between JAVA and CPP/IEEE.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        double a = 123.45676;
+        System.out.println("Hello World!");
+        System.out.print(Long.toHexString(Double.doubleToRawLongBits(a)));
+    }
+}
