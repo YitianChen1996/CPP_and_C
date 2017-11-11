90b0085a393e7d8039a7f6ed6f9d8bb2e054d4b5
YitianChen1996 <30911432+YitianChen1996@users.noreply.github.com>
2017-11-11 12:09:18 -0500

    Update longjmptest.c

--- Exception handle in C/longjmptest.c
+++ Exception handle in C/longjmptest.c
@@ -7,7 +7,7 @@ static jmp_buf env;
 void handler(int signnum) {
     if (signnum == SIGSEGV) {
         printf("segmentation fault\n");
-        longjmp(env, signnum);
+        longjmp(env, signnum);//when you go back, signum is going set the return value of setjmp(), and the program will go on at where the setjmp is.
     }
 }
 
@@ -23,16 +23,16 @@ void handler(int signnum) {
 int main() {
     int i, *p = NULL;
     double a, b, c;
-    signal(SIGSEGV, handler);
+    signal(SIGSEGV, handler);//only once, if the handler is called, and reach the same exception the next time, it will not handle it. You have to regist it again.
     //scanf("%lf%lf", &a, &b);
-    i = setjmp(env);
+    i = setjmp(env);//take a photo of the program's state
     if (i == 0) {
         *p = 1;
         //c = divide(a, b);
     } else if (i == SIGSEGV) {
         printf("segmentation fault, handle it\n");
-    } else if (i == 2) {
-        printf("too big\n");
+    } else if (i == 1) {
+        printf("divided by zero\n");
     }
     return 0;
 }
